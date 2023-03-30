a56bb67bdf49d59c553e0ff5061633a12dedaae9
Vu Ngan Yen <yen.vu@amitgroup.vn>
2023-03-30 13:55:43 +0700

    fix: UI Home

--- mobile/app/src/main/java/vn/vunganyen/mobile/data/api/PathURL.kt
+++ mobile/app/src/main/java/vn/vunganyen/mobile/data/api/PathURL.kt
@@ -3,6 +3,6 @@ package vn.vunganyen.mobile.data.api
 class PathURL {
     companion object{
         const val BASE_URL = "http://nganyen.ok.edu.vn/"
-            //"http://192.168.2.18:4000/"
+      //  const val BASE_URL = "http://192.168.2.18:4000/"
     }
 }
